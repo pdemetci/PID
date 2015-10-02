11657f4e646cd5ac72717633960545502c67b40a
InseongJoe <ijoe@joinseonguiair.olin.edu>
2015-10-01 22:04:35 -0400

    Add second motor
    Added second motor control.

--- PID.ino
+++ PID.ino
@@ -8,27 +8,51 @@
 # include "utility/Adafruit_PWMServoDriver.h"
 
 
-// ============================= Initial Settings ========================== //
 
-// Initial Constants
-int startSpeed = 50;
-int dir;
+// ================ Global Objects and Variables ==================== //
 
+// Input Pins
+const int analogInPin1 = A0; 
+const int analogInPin2 = A1;
+
+// Constants
+int startSpeed = 50; // default speed
+
+
+
+//Counters
+int dir; // counter variable for direction
+int current_pos // counter variable for current position of motor
 
 // =========================== Motor Settings ========================== //
 
+// Create motor shield object with default address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_DCMotor*myMotor = AFMS.getMotor(1);
+
+// Create two motors
+Adafruit_DCMotor *myMotor1 = AFMS.getMotor(1);
+Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
+
+// ========================= Sensor Methods ============================= //
+
+
+
+
+
+
 
 // ========================= Main Methods =============================== //
 void setup() {
+  Serial.begin(9600); //start serial connection
   AFMS.begin();
-  myMotor->setSpeed(startSpeed);
+  myMotor1->setSpeed(startSpeed); // Set initial speed of motor 1
+  myMotor2->setSpeed(startSpeed); // Set initial speed of motor 2
   dir = FORWARD;
 
 }
 
 void loop() {
-  myMotor->run(dir);
+  myMotor1->run(dir);
+  myMotor2->run(dir);
 
 }
