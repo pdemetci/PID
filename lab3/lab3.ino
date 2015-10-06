bc2a65e209985d2fd054bafbaf21916a86e7fe2e
InseongJoe <ijoe@joinseonguiair.olin.edu>
2015-10-05 20:30:06 -0400

    Added comments

--- lab3/lab3.ino
+++ lab3/lab3.ino
@@ -1,31 +1,32 @@
 // ============== Global Variables and Parameters ============= //
 
-const int analogInPin1 = A0;
+// Constant Variables 
+const int analogInPin1 = A0;  // Input pins
 const int analogInPin2 = A1;
+const int setPoint = 400;     // Set wanted default position
 
-const int setPoint = 400;
+// Counters
+int lSensor = 0;        // Variable for left sensor value
+int rSensor = 0;        // Variable for right sensor value
+int lSpeed = 0;         // Variable for left motor speed 
+int rSpeed = 0;         // Variable for right motor speed
 
-int lSensor = 0;
-int rSensor = 0;
+int errorVal = 0;       // Variable for error value
+int prevError = 0;      // Variable for previous error value
 
-int dir;
-int defSpeed = 50;
-
-int lSpeed = 0;
-int rSpeed = 0;
-
-int errorVal = 0;
-int prevError = 0;
-
+// Variables for PID
+float kp = 0.00;      // Variables for PID parameters
+float kd = 0.00;
+float ki = 0.00;
 int proportional = 0;
 int derivative = 0;
 int integral = 0;
+int turnVal = 0;      // Variable for total PID value
 
-int turnVal = 0;
+// Other Variables
+int dir;                // Variable for motor direction
+int defSpeed = 50;      // Set default speed of motors
 
-float kp = 0.00;
-float kd = 0.00;
-float ki = 0.00;
 
 // =========================== Motor Settings ========================== //
 
@@ -38,7 +39,8 @@ Adafruit_DCMotor *rMotor = AFMS.getMotor(2); //left wheel
 
  // ======================== Other Methods ================== //
 
-float pidCalc(){
+// Calculate PID value
+void pidCalc(){
   proportional = errorVal;
   integral += proportional;
   derivative = errorVal - prevError;
@@ -46,19 +48,25 @@ float pidCalc(){
   turnVal = proportional*kp + integral*ki + derivative*kd;
 }
 
+// Calculate sensor value and set speed according to value
 void readSensorVal(){
   lSensor = analogRead(analogInPin1);
   rSensor = analogRead(analogInPin2);
 
+// On center
   if (lSensor =< setPoint && rSensor =< setPoint){
     lSpeed = defSpeed;
     rSpeed = defSpeed;
   }
+
+// Tilted left
   if (lSensor =< setPoint && rSensor > setPoint){
     errorVal = rSensor - setPoint;
     lSpeed = defSpeed;
     rSpeed = defSpeed + turnVal;
   }
+
+// Tilted right
   if (lSensor > setPoint && rSensor =< setPoint){
     errorVal = lSensor - setPoint;
     lSpeed = defSpeed + turnVal;
@@ -78,6 +86,8 @@ void setup() {
 }
 
 void loop() {
+
+// Input values of PID parameters through serial port
   if (Serial.available() > 0){
       for (int x = 0; x<3; x++) {
         switch (x) {
@@ -97,9 +107,14 @@ void loop() {
           break;
         }
      }
-     
+
+  // Calculate PID
   pidCalc();
+
+  // Read sensor and set speed
   readSensorVal();
+
+  // Apply speed on motor
   lMotor->setSpeed(lSpeed);
   rMotor->setSpeed(rSpeed);
    
